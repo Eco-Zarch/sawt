3a96e887535809fd27afd0c36666bba31ff29401
Sydney Feldman <sydneyfeldman@Sydneys-Laptop.local>
2025-02-07 11:10:38 -0600

    download transcripts test

--- packages/pull/src/src.py
+++ packages/pull/src/src.py
@@ -2,7 +2,10 @@ import json
 from youtube_transcript_api import YouTubeTranscriptApi
 
 video_data = [
-    ['https://www.youtube.com/watch?v=j3wYgcTct2E', 'City Council Meeting 11/16/2023', '11/16/2023'],
+     ['https://www.youtube.com/watch?v=t8uqW_ZMYiE', 'Utility, Cable, Telecom, and Technology Committee Meeting 1/30/2024', '1/30/2024'] ]
+   # ['https://www.youtube.com/watch?v=6f_SNqcisq8', 'City Council Meeting 1/9/2025', '1/9/2025'] ]
+
+'''['https://www.youtube.com/watch?v=j3wYgcTct2E', 'City Council Meeting 11/16/2023', '11/16/2023'],
     ['https://www.youtube.com/watch?v=kbkCH8CLCws', 'City Council Meeting 12/1/2023', '12/1/2023'],
     ['https://www.youtube.com/watch?v=HB2HEqcf_rc', 'City Council Meeting 12/14/2023', '12/14/2023'],
     ['https://www.youtube.com/watch?v=_xvqfFhZEaw', 'City Council Meeting 12/14/2023', '12/14/2023'],
@@ -18,7 +21,8 @@ video_data = [
     ['https://www.youtube.com/watch?v=FF8FfduEA1Q', 'City Council Meeting 4/18/2024', '4/18/2024'],
     ['https://www.youtube.com/watch?v=yeJuIsl8od0', 'City Council Meeting 5/2/2024', '5/2/2024'],
     ['https://www.youtube.com/watch?v=Q2ocfAFWgd0', 'City Council Meeting 6/6/2024', '6/6/2024'],
-]
+    '''
+
 
 all_segments = []
 
@@ -72,7 +76,12 @@ for video_url, video_title, publish_date in video_data:
     all_segments.extend(segments)
 
 output = {'messages': all_segments}
+with open('packages/backend/src/json_fc_directory/fc_transcript_1_2024_test.json', 'w') as f:
+    json.dump(output, f, indent=2) 
+'''
+output = {'messages': all_segments}
 with open('../../backend/src/json_fc_directory/fc_transcript_11-2024_5-2024.json', 'w') as f:
     json.dump(output, f, indent=2)
 
 print('All transcript segments saved to afc_transcript_11-2024_5-2024.json')
+'''

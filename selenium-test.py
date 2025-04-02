f057226572985b305c29dca926c1bfbc41346a2e
Sydney Feldman <sydneyfeldman@Sydneys-Laptop.local>
2025-04-02 17:13:33 -0500

    test fix

--- selenium-test.py
+++ selenium-test.py
@@ -27,7 +27,7 @@ def get_mp4_links():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = "/usr/bin/chromium-browser"
+    chrome_options.binary_location = "/usr/bin/chromium-browser" #need to hash out when running locally
 
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(URL)

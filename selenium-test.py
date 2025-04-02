4f9c6dd9679632cb3be4710d150aea57baf9a7d2
Sydney Feldman <sydneyfeldman@Sydneys-Laptop.local>
2025-04-02 16:02:43 -0500

    test fix

--- selenium-test.py
+++ selenium-test.py
@@ -14,6 +14,7 @@ def get_mp4_links():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = "/usr/bin/chromium-browser"
 
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(URL)
